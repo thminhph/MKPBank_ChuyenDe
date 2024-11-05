c3e75db858685a19c3e5926d3e0e60625fa47c46
22211tt0826 <Phuyngyuen9484@gnmail.com>
2024-11-05 12:27:53 +0700

    cap nhat

--- DoAnPTUD/BLL/BLL_LoadValue.cs
+++ DoAnPTUD/BLL/BLL_LoadValue.cs
@@ -1,47 +1,42 @@
-﻿using DAL;
-using DTO;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using DAL;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace BUS
-{
-    public class BLL_LoadValue
-    {
-        DAL_LoadValue load = new DAL_LoadValue();
-        public IQueryable XemNganhChinh()
-        {
-            return load.XemNganhChinh();
-        }
-        public IQueryable XemNganh(int maNganhChinh)
-        {
-            return load.XemNganh(maNganhChinh);
-        }
-        public IQueryable XemNhanVien()
-        {
-            return load.XemNhanVien();
-        }
-        public string LayTenNganhChinh(int id)
-        {
-            return load.LayTenNganhChinh(id);
-        }
-        public string LayTenNganh(int id)
-        {
-            return load.LayTenNganh(id);
-        }
-        public string LayTenNV(string idNV)
-        {
-            return load.LayTenNV(idNV);
-        }
-        public List<DTO_KhachHang> HienThiDanhSachKH()
-        {
-            return load.HienThiDanhSachKH();
-        }
-        public List<DTO_KhachHang> HienThiDanhSachKH(Dictionary<string, string> whereArg)
-        {
-            return load.HienThiDanhSachKH(whereArg);
-        }
-    }
-}
+//namespace BUS
+//{
+//    public class BLL_LoadValue
+//    {
+//        DAL_LoadValue load = new DAL_LoadValue();
+//        public IQueryable XemNganhChinh()
+//        {
+//            return load.XemNganhChinh();
+//        }
+//        public IQueryable XemNganh(int maNganhChinh)
+//        {
+//            return load.XemNganh(maNganhChinh);
+//        }
+//        public IQueryable XemNhanVien()
+//        {
+//            return load.XemNhanVien();
+//        }
+//        public string LayTenNganhChinh(int id)
+//        {
+//            return load.LayTenNganhChinh(id);
+//        }
+//        public string LayTenNganh(int id)
+//        {
+//            return load.LayTenNganh(id);
+//        }
+//        public string LayTenNV(string idNV)
+//        {
+//            return load.LayTenNV(idNV);
+//        }
+//        public IQueryable InDSKhachHang(string[] arr)
+//        {
+//            return load.InDSKhachHang(arr);
+//        }
+//    }
+//}
--- DoAnPTUD/BLL/BLL_TaiKhoan.cs
+++ DoAnPTUD/BLL/BLL_TaiKhoan.cs
@@ -26,9 +26,6 @@ namespace BLL
         {
             return taiKhoan.GanThongTinNguoiDung(sdt,ten);
         }
-        public void Them(DTO_TaiKhoan tk)
-        {
-            taiKhoan.Them(tk);
-        }
+
     }
 }
